a73b6cdb94755e1663950d1d8b7d9132d66d3c2c
Keith McDonnell <keith@dancingtext.com>
2016-04-19 16:14:42 +0100

    /articles/1/comments/1 -> display comments. closes #4

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,8 @@
 module CommentsHelper
+  def avatar_url(email)
+    default_url = "https://robohash.org/#{request.remote_ip}.png"
+
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=75&d=#{CGI.escape(default_url)}"
+  end
 end
